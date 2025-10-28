ab25ea48dd071109577dc7d00addadda54d879d1
farhanintesar <farhanintesar0099@gmail.com>
2025-10-28 22:44:55 +0600

    Used 'for loop' for marging in Task 47

--- MargeTwoSortedArrayTask47.java
+++ MargeTwoSortedArrayTask47.java
@@ -8,24 +8,43 @@ public class MargeTwoSortedArrayTask47 {
         ArrayUtility.displayArray(mergedArr);
     }
 
-    public static int[] merge(int[] arr1,int[] arr2){
-        int newSize = arr1.length + arr2.length ;
-        int[] newArr = new int[newSize];
 
-        int i = 0, j = 0,k = 0;
-        while (i < arr1.length || i< arr2.length){
-            if ( j == arr2.length ||
-                    (i < arr1.length && arr1[i] < arr2[j]) ) {
-                newArr[k] = arr1[i];
-                i++;
-                k++;
-            } else {
-                newArr[k] = arr2[j];
-                k++;
-                j++;
-            }
-        }
+
+
+public static int[] merge(int[] arr1,int[] arr2) {
+    int[] newArr = new int[arr1.length + arr2.length] ;
+
+    for(int i = 0 ; i< arr1.length ; i++ ) {
+         newArr[i]=arr1[i];
+    }
+    for(int i = 0 ; i < arr2.length ; i++){
+        newArr[i + arr1.length] =arr2[i];
+    }
 
     return newArr;
-    }
+}
+
+
+
+
+    // public static int[] merge(int[] arr1,int[] arr2){
+    //     int newSize = arr1.length + arr2.length ;
+    //     int[] newArr = new int[newSize];
+
+    //     int i = 0, j = 0,k = 0;
+    //     while (i < arr1.length || i< arr2.length){
+    //         if ( j == arr2.length ||
+    //                 (i < arr1.length && arr1[i] < arr2[j]) ) {
+    //             newArr[k] = arr1[i];
+    //             i++;  
+    //             k++;
+    //         } else {
+    //             newArr[k] = arr2[j];
+    //             k++;
+    //             j++;
+    //         }
+    //     }
+
+    // return newArr;
+    // }
 }
